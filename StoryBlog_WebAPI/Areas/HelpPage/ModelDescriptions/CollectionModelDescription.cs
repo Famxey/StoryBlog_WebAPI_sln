28286464d83c9302bf3e00e4c74135edde072446
Famxey Cyan <642322033@qq.com>
2019-04-29 05:36:59 +0800

    添加项目文件。

---
+++ StoryBlog_WebAPI/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace StoryBlog_WebAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ StoryBlog_WebAPI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="StoryBlog_WebAPI.WebApiApplication" Language="C#" %>
